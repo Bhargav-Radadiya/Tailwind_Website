948cd7efb5b8c6ae12676fed45f6c6533071c3f1
Bhargav-Radadiya <bhargavradadiya11@gmail.com>
2023-07-03 10:15:30 +0530

    first commit

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config  = {
+    theme: {
+      fontFamily: {
+        'sans':["Poppins", "sans-seri"],
+        'serif':["roboto", "sans-seri"]
+        
+      },
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1140px',
+        '2xl': '1140px',
+      },
+    }
+  }
